10d98c304d3f17317e01dfacebe75c71c47bdfbe
Aziz Özenir <zyzoznr@gmail.com>
2024-01-30 17:47:03 +0300

    second

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using final.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace final.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
